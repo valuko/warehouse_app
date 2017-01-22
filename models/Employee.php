797b723d7336f1342b3a53b286a491983d723925
Victor Aluko <victor.aluko@gmail.com>
2017-01-22 22:54:59 +0200

    Defined the before save, find employee by username and validate password methods

--- models/Employee.php
+++ models/Employee.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use Yii;
+use yii\base\Security;
 use yii\db\ActiveRecord;
 
 /**
@@ -52,5 +53,41 @@ class Employee extends ActiveRecord
 
     public function getProducts() {
         return $this->hasMany(Product::className(), ['employee_id' =>'id']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeSave($insert) {
+        $this->password = Yii::$app->getSecurity()->generatePasswordHash($this->password);
+
+        return parent::beforeSave($insert);
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return Employee|null
+     */
+    public static function findByUsername($username)
+    {
+        $employee = self::find()->where(['username' => $username])->one();
+        if (empty($employee)) {
+            return null;
+        }
+
+        return $employee;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return Yii::$app->getSecurity()->validatePassword($password, $this->password);
     }
 }
