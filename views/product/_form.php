e7c5fb3549fc41d4bb3391e7f2a0c747c80e56d2
Victor Aluko <victor.aluko@gmail.com>
2017-01-23 01:28:50 +0200

    Created the Product manager crud

---
+++ views/product/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use dosamigos\ckeditor\CKEditor;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Product */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'quantity')->textInput() ?>
+
+    <?= $form->field($model, 'price')->textInput() ?>
+
+    <?= $form->field($model, 'description')->widget(CKEditor::className(), [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ]) ?>
+
+    <?= $form->field($model, 'image_path')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'employee_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
