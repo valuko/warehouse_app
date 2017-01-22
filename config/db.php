5c00e7e1ecaaeebd34af8ce57d4d5a44db9cdcd9
Victor Aluko <victor.aluko@gmail.com>
2017-01-22 20:38:58 +0200

    Changed the DB name. config error

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=warehouse_db',
+    'dsn' => 'mysql:host=localhost;dbname=warehouse_app',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
