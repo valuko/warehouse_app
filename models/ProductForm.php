5e3820bc9b1a771fd7cf3bcfcad6bf8524e1671c
Victor Aluko <victor.aluko@gmail.com>
2017-01-23 03:10:11 +0200

    Implemented necessary validation rules and upload validation

--- models/ProductForm.php
+++ models/ProductForm.php
@@ -11,17 +11,48 @@ namespace app\models;
 use Yii;
 use yii\base\Model;
 use yii\helpers\ArrayHelper;
+use yii\web\UploadedFile;
 
 class ProductForm extends Model
 {
+    /**
+     * @var integer
+     */
     public $id;
+    /**
+     * @var string
+     */
     public $name;
+    /**
+     * @var string
+     */
     public $description;
+    /**
+     * @var integer
+     */
     public $quantity;
+    /**
+     * @var double
+     */
     public $price;
+    /**
+     * @var UploadedFile
+     */
     public $image_path;
-    public $categories;
+    /**
+     * @var array
+     */
+    public $category_ids;
+    /**
+     * @var integer
+     */
+    public $employee_id;
+    /**
+     * @var boolean
+     */
     public $isNewRecord;
+
+    public $categories;
 
 
     /**
@@ -35,6 +66,8 @@ class ProductForm extends Model
             // extra type validations
             ['quantity', 'integer'],
             ['price', 'double'],
+            ['category_ids', 'each', 'rule' => ['integer']],
+            [['image_path'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg,jpeg'],
         ];
     }
 
@@ -64,5 +97,49 @@ class ProductForm extends Model
             // Silently handle the exception here
             return null;
         }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validate($attributeNames = null, $clearErrors = true)
+    {
+        if (!parent::validate($attributeNames, $clearErrors)) {
+            return false;
+        }
+
+        // Validate the category IDs
+        $this->categories = Category::findAll($this->category_ids);
+        if (count($this->categories) != count($this->category_ids)) {
+            $this->addError('category_ids', 'Some categories are not valid');
+            return false;
+        }
+        if (!$this->upload()) {
+            $this->addError('image_path', 'Image upload failed');
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Handles the image file upload
+     * @return bool Indicates successful image upload
+     */
+    private function upload()
+    {
+        return $this->image_path->saveAs('uploads/' . $this->image_path->baseName . '.' . $this->image_path->extension);
+    }
+
+    public function save()
+    {
+
+        // Populate the appropriate model and then save
+        $product = new Product();
+        // Prefill
+        $categoryIds = $this->category_ids;
+        $product->load($this->attributes);
+
+        //$user->link('markets', $market);
     }
 }
